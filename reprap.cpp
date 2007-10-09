e26c8611d8637488e50042a3ef3f35fcadd5b8da
hoekstar <hoekstar@cb376a5e-1013-0410-a455-b6b1f9ac8223>
2007-10-09 20:40:22 +0000

    i got my repduino firmware code to compile.  now i just have to test it!!.
    git-svn-id: https://reprap.svn.sourceforge.net/svnroot/reprap/trunk/users/hoeken/arduino-code/repduino-firmware@984 cb376a5e-1013-0410-a455-b6b1f9ac8223

--- reprap.cpp
+++ reprap.cpp
@@ -1,39 +1,50 @@
 /******************
+* Get our libraries
+******************/
+#include <RepStepper.h>
+
+// library interface description
+struct Point {
+	int x;
+	int y;
+	int z;
+};
+
+/******************
 *  Definitions
 ******************/
 #define POINT_QUEUE_SIZE 64
 
-/******************
-* digital i/o
-******************/
-//x axis stuff.
-byte x_home_pin = 2;
-byte x_dir_pin = 7;
-byte x_step_pin = 8;
+/****************************
+* digital i/o pin assignment
+****************************/
 
-//y axis stuff
-byte y_home_pin = 3;
-byte y_dir_pin = 9;
-byte y_step_pin = 10;
+//these are our stepper pins
+#define X_DIR_PIN 7
+#define X_STEP_PIN 8
+#define Y_DIR_PIN 9
+#define Y_STEP_PIN 10
+#define Z_DIR_PIN 12
+#define Z_STEP_PIN 13
 
-//z axis stuff
-byte z_home_pin = 11;
-byte z_dir_pin = 12;
-byte z_step_pin = 13;
+//pins for our limit switches
+#define X_HOME_PIN 2
+#define Y_HOME_PIN 3
+#define Z_HOME_PIN 11
 
 //for our extruder
-byte extruder_motor_dir_pin = 4;
-byte extruder_motor_speed_pin = 5;
-byte extruder_heater_pin = 6;
+#define EXTRUDER_MOTOR_DIR_PIN 4
+#define EXTRUDER_MOTOR_SPEED_PIN 5
+#define EXTRUDER_HEATER_PIN 6
 
 /******************
 * analog inputs
 ******************/
-byte extruder_thermistor_pin = 0;
-byte x_encoder_pin = 1;
-byte y_encoder_pin = 2;
-byte z_encoder_pin = 3;
-byte extruder_motor_encoder_pin = 4;
+#define EXTRUDER_THERMISTOR_PIN 0
+#define X_ENCODER_PIN 1
+#define Y_ENCODER_PIN 2
+#define Z_ENCODER_PIN 3
+#define EXTRUDER_MOTOR_ENCODER_PIN 4
 
 //our analog sensor values.
 int thermistor_reading = 0;
@@ -42,20 +53,10 @@ int y_encoder_reading = 0;
 int z_encoder_reading = 0;
 int extruder_motor_encoder_reading = 0;
 
-//information on our axes
-int x_position = 0;
-int y_position = 0;
-int z_position = 0;
-
 //our endstop variables
 bool x_at_home = 0;
 bool y_at_home = 0;
 bool z_at_home = 0;
-
-//what direction are we going?
-bool x_direction = HIGH;
-bool y_direction = HIGH;
-bool z_direction = HIGH;
 
 //our extruder info
 bool extruder_direction = HIGH;
@@ -67,46 +68,40 @@ byte target_thermistor_value = 0;
 byte point_index = 0;
 Point point_queue[POINT_QUEUE_SIZE];
 Point target_point;
+Point current_position;
+
+//here are our stepper objects
+RepStepper x_stepper(200, X_DIR_PIN, X_STEP_PIN);
+RepStepper y_stepper(200, Y_DIR_PIN, Y_STEP_PIN);
+RepStepper z_stepper(200, Z_DIR_PIN, Z_STEP_PIN);
 
 void setup()
 {
-	//these inputs are our limit switches
-	pinMode(x_home_pin, INPUT);
-	pinMode(y_home_pin, INPUT);
-	pinMode(z_home_pin, INPUT);
-	
-	//these outputs control the direction of our steppers
-	pinMode(x_dir_pin, OUTPUT);
-	pinMode(y_dir_pin, OUTPUT);
-	pinMode(z_dir_pin, OUTPUT);
+	//fire up our serial comms.
+	Serial.begin(57600);
+	Serial.println("RepDuino v1.0 started up.");
 
-	//these outputs are how we step our steppers
-	pinMode(x_step_pin, OUTPUT);
-	pinMode(y_step_pin, OUTPUT);
-	pinMode(z_step_pin, OUTPUT);
+	//these inputs are our limit switches
+	pinMode(X_HOME_PIN, INPUT);
+	pinMode(Y_HOME_PIN, INPUT);
+	pinMode(Z_HOME_PIN, INPUT);
 	
 	//these outputs control our extruder
-	pinMode(extruder_motor_dir_pin, OUTPUT);
-	pinMode(extruder_motor_speed_pin, OUTPUT);
-	pinMode(extruder_heater_pin, OUTPUT);	
-
-	Serial.begin(57600);
+	pinMode(EXTRUDER_MOTOR_DIR_PIN, OUTPUT);
+	pinMode(EXTRUDER_MOTOR_SPEED_PIN, OUTPUT);
+	pinMode(EXTRUDER_HEATER_PIN, OUTPUT);	
 }
+
 
 void loop()
 {
-	Serial.println("RepDuino v1.0 started up.");
-	
-	while (1)
-	{
-		receiveCommands();
-		readSensors();
-		updateStatus();
-		executeCommands();
-	}
+	receiveCommands();
+	readSensors();
+	updateStatus();
+	executeCommands();
 }
 
-void function receiveCommands()
+void receiveCommands()
 {
 	int incoming;
 	
@@ -141,26 +136,27 @@ void function receiveCommands()
 	}
 }
 
-void function readSensors()
+void readSensors()
 {
 	//just read in all our sensor data... make it easy.
-	thermistor_reading = analogRead(extruder_thermistor_pin);
-	x_encoder_reading = analogRead(x_encoder_pin);
-	y_encoder_reading = analogRead(y_encoder_pin);
-	z_encoder_reading = analogRead(z_encoder_pin);
-	extruder_motor_encoder_reading = analogRead(extruder_motor_encoder_pin);
+	thermistor_reading = analogRead(EXTRUDER_THERMISTOR_PIN);
+	x_encoder_reading = analogRead(X_ENCODER_PIN);
+	y_encoder_reading = analogRead(Y_ENCODER_PIN);
+	z_encoder_reading = analogRead(Z_ENCODER_PIN);
+	extruder_motor_encoder_reading = analogRead(EXTRUDER_MOTOR_ENCODER_PIN);
 	
 	//also check our endstops as well
-	x_at_home = digitalRead(x_home_pin);
-	y_at_home = digitalRead(y_home_pin);
-	z_at_home = digitalRead(z_home_pin);
+	x_at_home = digitalRead(X_HOME_PIN);
+	y_at_home = digitalRead(Y_HOME_PIN);
+	z_at_home = digitalRead(Z_HOME_PIN);
 }
 
-void function updateStatus()
+void updateStatus()
 {
 	//let them know its our status line.
 	//Serial.print('Status:');
-	
+
+/*	
 	//our analog readings.
 	Serial.print("T:");
 	Serial.print(thermistor_reading);
@@ -205,28 +201,112 @@ void function updateStatus()
 	Serial.print('Heater:');
 	Serial.print(extruder_heater_pwm);
 	Serial.print();
-	
+*/	
 	//end our data transmission
 	Serial.println('!');
 }
 
-void function executeCommands()
+void executeCommands()
 {
+	checkExtruder();
+	checkCartesianBot();
 }
 
-void function printVersion()
+void checkExtruder()
+{
+	//set the direction of our extruder
+	if (extruder_direction)
+		digitalWrite(EXTRUDER_MOTOR_DIR_PIN, HIGH);
+	else
+		digitalWrite(EXTRUDER_MOTOR_DIR_PIN, LOW);
+
+	//set the speed of our motor
+	analogWrite(EXTRUDER_MOTOR_SPEED_PIN, extruder_speed);
+
+	//check our temp.
+	checkThermostat();
+}
+
+void checkThermostat()
+{
+	extruder_heater_pwm = calculateHeaterPWM();
+	
+	analogWrite(EXTRUDER_HEATER_PIN, extruder_heater_pwm);
+}
+
+byte calculateHeaterPWM()
+{
+	if (thermistor_reading < target_thermistor_value)
+		return 255;
+	else
+		return 0;
+}
+
+struct Point unqueuePoint()
+{
+	Point temp;
+	
+	//get our first point.
+	temp = point_queue[0];
+	
+	//shift the array down now.
+	for (int i=0; i<point_index-1; i++)
+		point_queue[i] = point_queue[i+1];
+	point_index++;
+	
+	//send it!
+	return temp;
+}
+
+void checkCartesianBot()
+{
+	//if we're at our point, get a new one!
+	if (atPoint(target_point))
+	{
+		//if we have any points left, get one!
+		if (point_index > 0)
+			target_point = unqueuePoint();
+		else
+			return;
+	}
+
+	//okay, now step to this!
+	stepToPoint(target_point);
+}
+
+bool atPoint(struct Point &target)
+{
+	if (target.x == current_position.x && target.y == current_position.y && target.z == current_position.z)
+		return true;
+	else
+		return false;
+}
+
+void stepToPoint(struct Point &target)
+{
+	if (target.x != current_position.x)
+		x_stepper.step();
+	
+	if (target.y != current_position.y)
+		y_stepper.step();
+		
+	if (target.z != current_position.z)
+		z_stepper.step();
+}
+
+void printVersion()
 {
 	Serial.println("RepDuino v1.0");
 }
 
-void function queuePoint()
+void queuePoint()
 {
 	if(point_index < (POINT_QUEUE_SIZE - 1))
 	{
 		//read in our points.
-		point_queue[point_index]->x = readInt();
-		point_queue[point_index]->y = readInt();
-		point_queue[point_index]->z = readInt();
+		point_queue[point_index].x = readInt();
+		point_queue[point_index].y = readInt();
+		point_queue[point_index].z = readInt();
 		
 		//move our pointer forward.
 		point_index++;
@@ -235,7 +315,7 @@ void function queuePoint()
 		Serial.println("Point queue is full!");
 }
 
-int function readInt()
+int readInt()
 {
 	int tmp;
 
@@ -247,44 +327,44 @@ int function readInt()
 	return tmp;
 }
 
-void function abortPrint()
+void abortPrint()
 {
 	//set temp to 0
 	//turn off all motors
 }
 
-void function readExtruderTemperature()
+void readExtruderTemperature()
 {
-	setExtruderTemperature(Serial.read());
+	setExtruderTargetTemperature(Serial.read());
 }
 
-void function setExtruderTemperature(byte temp)
+void setExtruderTargetTemperature(byte temp)
 {
 	target_thermistor_value = temp;
 }
 
-void function goHome()
+void goHome()
 {
 	//clear our queue, and tell it to go home.
 	//the endstops will take care of the rest. (hopefully)
 	point_index = 0;
-	point_queue[0]->x = -100;
-	point_queue[0]->y = -100;
-	point_queue[0]->z = -100;
+	point_queue[0].x = -100;
+	point_queue[0].y = -100;
+	point_queue[0].z = -100;
 }
 
-void function readExtruderSettings()
+void readExtruderSettings()
 {
 	setExtruderDirection(Serial.read());
 	setExtruderSpeed(Serial.read());
 }
 
-void function setExtruderDirection(bool dir)
+void setExtruderDirection(bool dir)
 {
 	extruder_direction = dir;
 }
 
-void function setExtruderSpeed(byte speed)
+void setExtruderSpeed(byte speed)
 {
 	extruder_speed = speed;	
 }
